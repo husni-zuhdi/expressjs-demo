1634a0285dc7bd3653055ca44c25f1cc05adf5bb
husnizuhdi <husni.zuhdi@tokopedia.com>
2022-06-04 18:26:37 +0700

    Migrate to Mysql Cloud Database

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,12 @@
 'use strict';
-
+require("dotenv").config()
 const mysql = require('mysql2');
 
 // database configuration
 const config = {
-    host: '0.0.0.0',
-    user: 'root',
-    password: 'passw0rd',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_ROOT_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
     database: 'expressjs_demo_db'
 }
 
